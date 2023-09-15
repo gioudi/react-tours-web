b0d50fe2dbc4f65e1befb511022b2c7e49bb8f0e
gioudi <sergiopenagos881@gmail.com>
2023-09-15 01:13:25 -0500

    feat/add_font_awesome

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { socialLinks } from '../data'
+import SocialLinks from './SocialLinks'
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div class="content has-text-centered">
-      {/* <SocialLinks data={socialLinks} parentClass='footer-icons' itemClass='footer-icon'> </SocialLinks> */}
+     <SocialLinks data={socialLinks} parentClass='footer-icons' itemClass='footer-icon'> </SocialLinks> 
     <p className="copyright">
       copyright &copy; Grupo travel tours company
       <span className='ml-3' id="date">{new Date().getFullYear()}</span> all rights reserved
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,11 @@ import React from "react"
 import ReactDOM  from "react-dom/client"
 import App from './App'
 import 'bulma/css/bulma.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
+
+library.add(fas)
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
