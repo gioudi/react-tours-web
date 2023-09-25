de613570bdafcdb328f67c10c4890ac6d4d5942a
gioudi <sergiopenagos881@gmail.com>
2023-09-25 12:22:02 -0500

    feat/add_unsplash-js

---
+++ src/components/TourCard.tsx
@@ -0,0 +1,23 @@
+import React, { FC, Fragment} from 'react'
+import { Tour } from '../interfaces/index';
+
+const TourCard: React.FC<{ payload: Tour }> = ({ payload }) => {
+    const { user, urls, links, alt_description, created_at } = payload;
+  
+    return (
+      <Fragment>
+         <div className="column">
+           <article className="card">
+          <div className="card-image">
+            <figure className="image is-4by3">
+            <img src={urls.full} className="" alt={alt_description} />
+            </figure>
+            <h6 className="subtitle is-6 px-4">{created_at}</h6>
+          </div>
+        </article>
+        </div>
+      </Fragment>
+    );
+}
+
+export default TourCard;
